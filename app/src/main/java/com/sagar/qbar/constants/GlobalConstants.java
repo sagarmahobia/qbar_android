164a55f9ca5bb9f6613bf237fa87638035b64d8a
Sagar Mahobia <sagarmahobia5@gmail.com>
2019-02-11 01:33:11 +0530

    fixed color on about page.

--- app/src/main/java/com/sagar/qbar/constants/GlobalConstants.java
+++
@@ -1,9 +0,0 @@
-package com.sagar.qbar.constants;
-
-/**
- * Created by SAGAR MAHOBIA on 08-Aug-18. at 15:29
- */
-public interface GlobalConstants {
-    String EXIT_APP_COUNT = "exit_app_count";
-    String OPENED_RATE_APP = "open_rate_app";
-}
